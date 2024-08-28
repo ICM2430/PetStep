1147dbcc29c382eaa00a66221a2f9d68233e9ce6
TatisVivas <t_vivas@javeriana.edu.co>
2024-08-28 00:55:03 -0500

    entrega1
    Co-Authored-By: Juan Canon <canonjp@javeriana.edu.co>
    Co-Authored-By: Perezpdaniel <176346831+Perezpdaniel@users.noreply.github.com>
    Co-Authored-By: Daniel Sandoval <56854119+rogeliorichmanastronaut@users.noreply.github.com>
    Co-Authored-By: AndresCRincon <124530465+andrescrincon@users.noreply.github.com>

---
+++ app/src/main/java/com/example/petstep/HomeWalkerActivity.kt
@@ -0,0 +1,20 @@
+package com.example.petstep
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomeWalkerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home_walker)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/example/petstep/PerfilActivity.kt
@@ -0,0 +1,20 @@
+package com.example.petstep
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PerfilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_perfil)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
