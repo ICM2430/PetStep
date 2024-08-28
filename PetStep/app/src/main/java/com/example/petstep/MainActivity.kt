f3f04998cf1217b7f76b097086cbacb5e67b5ddb
TatisVivas <t_vivas@javeriana.edu.co>
2024-08-28 08:23:10 -0500

    separación de proyecto

---
+++ PetStep/app/src/main/java/com/example/petstep/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.petstep
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.petstep.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.iniciarsesionbutton.setOnClickListener {
+            startActivity(Intent(baseContext,IniciarSesionActivity::class.java))
+        }
+
+        binding.registrarseButtom.setOnClickListener {
+            startActivity(Intent(baseContext,RegistroActivity::class.java))
+        }
+    }
+}
