62f6a09f3eb47502781556968e01ecf9d72a7431
AndresCRincon <124530465+AndresCRincon@users.noreply.github.com>
2024-10-16 01:29:37 -0500

    Update IniciarSesionActivity.kt con opcion para huella

--- PetStep/app/src/main/java/com/example/petstep/IniciarSesionActivity.kt
+++ PetStep/app/src/main/java/com/example/petstep/IniciarSesionActivity.kt
@@ -5,10 +5,18 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
 import com.example.petstep.databinding.ActivityIniciarSesionBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.database.*
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import java.util.concurrent.Executor
 
 class IniciarSesionActivity : AppCompatActivity() {
 
@@ -16,6 +24,8 @@ class IniciarSesionActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var database: FirebaseDatabase
     private lateinit var userRef: DatabaseReference
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
     val TAG = "FIREBASE_APP"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,22 +36,85 @@ class IniciarSesionActivity : AppCompatActivity() {
         auth = FirebaseAuth.getInstance()
         database = FirebaseDatabase.getInstance()
 
-        if(auth.currentUser != null){
-            //entrar dependiendo su rol
+        if (auth.currentUser != null) {
+            // Entrar dependiendo su rol
+            Log.d(TAG, "User already logged in, updating UI.")
             updateUI(auth.currentUser)
         }
 
         binding.registrese.setOnClickListener {
             startActivity(Intent(baseContext, SelectionActivity::class.java))
         }
+
         binding.ingresarButtom.setOnClickListener {
             val email = binding.correo.text.toString()
             val password = binding.contrasena.text.toString()
 
+            Log.d(TAG, "Botón iniciar sesión presionado")
             if (validateForm(email, password)) {
+                Log.d(TAG, "Formulario validado, intentando iniciar sesión.")
                 loginUser(email, password)
             }
         }
+
+        setupBiometricAuthentication()
+        binding.biometricButton.setOnClickListener {
+            val biometricManager = BiometricManager.from(this)
+            if (biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG) == BiometricManager.BIOMETRIC_SUCCESS) {
+                Log.d(TAG, "Biometric authentication available, prompting user.")
+                biometricPrompt.authenticate(promptInfo)
+            } else {
+                Log.e(TAG, "Biometric authentication is not available or not set up.")
+                Toast.makeText(this, "Biometric authentication is not available", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun setupBiometricAuthentication() {
+        val biometricManager = BiometricManager.from(this)
+        when (biometricManager.canAuthenticate()) {
+            BiometricManager.BIOMETRIC_SUCCESS -> Log.d(TAG, "App can authenticate using biometrics.")
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> Log.e(TAG, "No biometric hardware available.")
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> Log.e(TAG, "Biometric features are currently unavailable.")
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> Log.e(TAG, "The user hasn't enrolled any biometric credentials.")
+        }
+
+        val executor: Executor = ContextCompat.getMainExecutor(this)
+        biometricPrompt = BiometricPrompt(this, executor, object : BiometricPrompt.AuthenticationCallback() {
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                super.onAuthenticationError(errorCode, errString)
+                Toast.makeText(applicationContext, "Authentication error: $errString", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(result)
+                Toast.makeText(applicationContext, "Authentication succeeded!", Toast.LENGTH_SHORT).show()
+                Log.d(TAG, "Biometric authentication succeeded, attempting Firebase login with saved credentials.")
+                // Llama a la función para iniciar sesión en Firebase después de la autenticación exitosa.
+                val sharedPref = getSharedPreferences("AuthPrefs", MODE_PRIVATE)
+                val savedEmail = sharedPref.getString("email", null)
+                val savedPassword = sharedPref.getString("password", null)
+
+                if (savedEmail != null && savedPassword != null) {
+                    Log.d(TAG, "Saved credentials found, logging in.")
+                    loginUser(savedEmail, savedPassword, isBiometric = true)
+                } else {
+                    Log.e(TAG, "No saved credentials found.")
+                    Toast.makeText(applicationContext, "No saved credentials available for biometric login", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onAuthenticationFailed() {
+                super.onAuthenticationFailed()
+                Toast.makeText(applicationContext, "Authentication failed", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Autenticación biométrica")
+            .setSubtitle("Inicia sesión con tu huella dactilar")
+            .setNegativeButtonText("Usar contraseña")
+            .build()
     }
 
     private fun updateUI(currentUser: FirebaseUser?) {
@@ -50,42 +123,58 @@ class IniciarSesionActivity : AppCompatActivity() {
             val duenoRef = database.getReference("users/duenos").child(userId)
             val paseadorRef = database.getReference("users/paseadores").child(userId)
 
+            Log.d(TAG, "Attempting to retrieve user role for userId: $userId")
+
             duenoRef.addListenerForSingleValueEvent(object : ValueEventListener {
                 override fun onDataChange(dataSnapshot: DataSnapshot) {
                     if (dataSnapshot.exists()) {
-                        startActivity(Intent(this@IniciarSesionActivity, HomeOwnerActivity::class.java))
+                        Log.d(TAG, "User is a 'dueno', navigating to HomeOwnerActivity.")
+                        navigateToActivity(HomeOwnerActivity::class.java)
                     } else {
                         paseadorRef.addListenerForSingleValueEvent(object : ValueEventListener {
                             override fun onDataChange(dataSnapshot: DataSnapshot) {
                                 if (dataSnapshot.exists()) {
-                                    startActivity(Intent(this@IniciarSesionActivity, HomeWalkerActivity::class.java))
+                                    Log.d(TAG, "User is a 'paseador', navigating to HomeWalkerActivity.")
+                                    navigateToActivity(HomeWalkerActivity::class.java)
                                 } else {
+                                    Log.d(TAG, "User role not found")
                                     Toast.makeText(baseContext, "User role not found", Toast.LENGTH_SHORT).show()
                                 }
                             }
 
                             override fun onCancelled(databaseError: DatabaseError) {
-                                Log.e(TAG, "Error: ${databaseError.message}")
+                                Log.e(TAG, "Error retrieving 'paseador' role: ${databaseError.message}")
                             }
                         })
                     }
                 }
 
                 override fun onCancelled(databaseError: DatabaseError) {
-                    Log.e(TAG, "Error: ${databaseError.message}")
+                    Log.e(TAG, "Error retrieving 'dueno' role: ${databaseError.message}")
                 }
             })
         }
     }
 
-    private fun loginUser(email: String, password: String) {
+    private fun loginUser(email: String, password: String, isBiometric: Boolean = false) {
         auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
             if (it.isSuccessful) {
+                Log.d(TAG, "Login successful, updating UI.")
+                // Guardar credenciales para autenticación biométrica solo si no es autenticación biométrica
+                if (!isBiometric) {
+                    val sharedPref = getSharedPreferences("AuthPrefs", MODE_PRIVATE)
+                    with(sharedPref.edit()) {
+                        putString("email", email)
+                        putString("password", password)
+                        apply()
+                    }
+                    Log.d(TAG, "Credentials saved for biometric authentication.")
+                }
                 updateUI(auth.currentUser) // never null if authentication is successful
             } else {
                 val message = it.exception?.message
                 Toast.makeText(baseContext, message, Toast.LENGTH_SHORT).show()
-                Log.e(TAG, "Error: $message")
+                Log.e(TAG, "Login error: $message")
                 binding.correo.text.clear()
                 binding.contrasena.text.clear()
             }
@@ -111,5 +200,11 @@ class IniciarSesionActivity : AppCompatActivity() {
     private fun validEmailAddress(email: String): Boolean {
         val regex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\$"
         return email.matches(regex.toRegex())
+    }
+
+    private fun navigateToActivity(activityClass: Class<*>) {
+        val intent = Intent(this, activityClass)
+        startActivity(intent)
+        finish()
     }
 }
