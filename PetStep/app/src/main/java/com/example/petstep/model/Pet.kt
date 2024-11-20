d214f6abbc6d1c7e83b6892fdbe035e31725c1eb
RogelioRichmanAstronaut <dvsandoval@javeriana.edu.co>
2024-11-20 01:49:35 -0500

    test

--- PetStep/app/src/main/java/com/example/petstep/model/Pet.kt
+++
@@ -1,8 +0,0 @@
-package com.example.petstep.adapters.com.example.petstep.model
-
-data class Pet(
-    val id: String = "",
-    val nombre: String = "",
-    val tipo: String = "",
-    val edad: Int = 0
-)
